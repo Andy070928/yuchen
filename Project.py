e384e6a4d02827aa97367607cdf6000fcbb151b6
Fernandididinho <frodriguez@putnamscience.org>
2025-04-03 09:15:48 -0400

    Project1
    ...

---
+++ Project.py
@@ -0,0 +1,11 @@
+print("Hello")
+print("You are being pursued by a stalker.Please do your best to increase your chances your chances of survival")
+print("You must ensure your health points,your staming points and you mental sanity points")
+
+health = 100 
+stamina = 100
+mentalsanity = 100
+print("Ps: if you health and stamina are less than 20,you will not able to run faster 0though you choose the fast speed")
+while True:
+    choice = input(health,stamina,mentalsanity)
+    if choice = 1
