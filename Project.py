9b00fcf5a3540462e31a6c2bfb888e6b13f7ea72
Fernandididinho <frodriguez@putnamscience.org>
2025-04-08 09:12:51 -0400

    project

--- Project.py
+++ Project.py
@@ -4,8 +4,16 @@ print("You must ensure your health points,your staming points and you mental san
 
 health = 100 
 stamina = 100
+
+
 mentalsanity = 100
 print("Ps: if you health and stamina are less than 20,you will not able to run faster 0though you choose the fast speed")
 while True:
     choice = input(health,stamina,mentalsanity)
-    if choice = 1
+    if choice == health: 
+        print("the healthy decreased", health)    
+    elif choice == stamina:
+        print("the stamina decreased",stamina)
+    elif choice == mentalsanity:
+        print("the mentalsanity decreased",mentalsanity)
+        
